16bff82314d572debc445072e1b8709b4e643b34
Raphael Norwitz <raphael.s.norwitz@columbia.edu>
2015-11-13 16:34:01 -0500

    added latest version of scanner, parser and ast with type declaration

--- ast.ml
+++ ast.ml
@@ -8,7 +8,7 @@ type program = stmt list * fdecl list
 
 type op = Plus | Minus | Times | Divide | Equal | Neq | Less | Leq | Greater | Geq
 type note_type = S | E |Q | H | W
-type declare_type = Int | Note | String | Song | Phrase | Measure
+type declare_type = Int | Note | String | Song | Phrase | Measure | List
 
 type invocation =
 	FunkCall of string * expr list
