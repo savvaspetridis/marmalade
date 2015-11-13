a5fe499d742543423787e16c7fb14f5578657272
Savvas Petridis <sdp2137@columbia.edu>
2015-11-12 23:10:28 -0500

    essential lines

--- compile.ml
+++ compile.ml
@@ -1,1 +1,11 @@
-<=====3
+(* open Sast *)
+open Ast
+
+let rec to_java = 
+	"import java.util.Arrays;\n" ^
+	"import java.util.ArrayList;\n" ^ 
+	"public class marmalade{\n" ^
+
+	"\n\npublic static void main(String[] args) {\n" ^
+		String.concat "\n" ()
+
