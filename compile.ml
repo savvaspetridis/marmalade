b3a7ee5ecfa2b8ee6facb31759907fc6f60b0323
Raphael Norwitz <raphael.s.norwitz@columbia.edu>
2015-11-13 22:10:27 -0500

    added methods to compile to deal with basic functions, song notes and length

--- compile.ml
+++ compile.ml
@@ -5,6 +5,10 @@ let to_java marma =
 	"import java.util.Arrays;\n" ^
 	"import java.util.ArrayList;\n" ^ 
 	"public class marmalade{\n" ^
+	"import jm.JMC;\n" ^
+	"import jm.music.data.*;\n" ^
+	"import jm.util.Play;\n" ^
+
 
 	"\n\npublic static void main(String[] args) {\n" ^
 		String.concat "\n" (List.map stmt_write marma.stmts) ^
@@ -25,17 +29,16 @@ let stmt_write = function
 | 	S_If(elif_l, s) -> writeIf elif_l s 
 | 	S_For(e1, e2, s) -> writeForLoop e1 e2 s
 | 	S_While(e, s) -> writeWhileLoop e s *)
-VarDecl(v) -> write_var_decl v
-| Expr(e) -> write_expr e
+VarDecl(v) -> write_var_decl v ^ ";\n"
+| Expr(e) -> write_expr e ^ ";\n"
 
 let write_var_decl v = 
 	match v with 
 	Assign(typ, id, exp) -> write_assign id exp typ
-	| 
 
 let write_assign i e t = 
 	match e with 
-	_ -> write_type e ^ " " ^ i ^ " = " ^ write_expr e
+	_ -> write_type t ^ " " ^ i ^ " = " ^ write_expr e
 
 let write_type ty =
 	match ty with
@@ -48,6 +51,8 @@ let write_type ty =
 	| List -> "ArrayList<Our_Object>"
 
 
+
+
 let write_expr e = 
 	match e with
 	IntLit(i, t) -> string_of_int i
@@ -55,6 +60,24 @@ let write_expr e =
 	| Id(x, t) -> x
 	| Binop(e_1, op, e_2, t) -> write_bin_op e_1 op e_2 t
 	| BasicList(l) -> "[" ^ List.map (fun e -> write_expr e ^ "," ) l ^  "]"
+	| Note_S(nt, dr) -> "new Note(" ^ string_of_int nt ^ ", " ^ write_rhythm dr  ^ ")"
+	| FuncList(funk_args, l) -> (List.map2 map_calls funk_args l)) ^ write_expr l
+
+
+let mapcall func param = 
+	match func with
+		"print" -> "System.out.println(" ^ param ^ ");\n"
+		| "play" -> "Play.midi(" ^ write_expr param ^ ");\n"
+
+let write_rhythm dr =
+			match dr with 
+			S -> "0.25"
+			| E -> "0.5"
+			| Q -> "1.0"
+			| H -> "1.5"
+			| W -> "2.0"
+
+
 
 let write_bin_op ex1 op ex2 typ = 
 	let e1 = write_expr ex1 and e2 = write_expr ex2 in
