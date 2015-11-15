409e4dde459bf34ba66ed72a81da22843b84477b
Raphael Norwitz <raphael.s.norwitz@columbia.edu>
2015-11-14 20:37:49 -0500

    added rec and changed let's to and's so that all functions in file are recognized by eachother. I also changed the interpretation of the Binop evaluation to reflect ast.ml

--- compile.ml
+++ compile.ml
@@ -1,7 +1,7 @@
 (* open Sast *)
 open Ast
 
-let to_java marma =
+let rec to_java marma =
 	"import java.util.Arrays;\n" ^
 	"import java.util.ArrayList;\n" ^ 
 	"public class marmalade{\n" ^
@@ -12,11 +12,11 @@ let to_java marma =
 
 	"\n\npublic static void main(String[] args) {\n" ^
 		String.concat "\n" (List.map stmt_write marma.stmts) ^
-		"\n}\n}"
+		"\n}\n}" 
 
 (* Below: From Fry *)
 
-let stmt_write = function
+and stmt_write stmt = function
 (*	S_Block(_,ss) -> "{\n" ^ String.concat "\n" ( List.rev (List.map j_stmt ss)) ^ "\n}"	
 | 	S_Expr(e,_) -> j_expr e ^ ";"
 |   S_Return(e,_) -> "return " ^ j_expr e ^";"
@@ -26,15 +26,15 @@ let stmt_write = function
 VarDecl(v) -> write_var_decl v ^ ";\n"
 | Expr(e) -> write_expr e ^ ";\n"
 
-let write_var_decl v = 
+and write_var_decl v = 
 	match v with 
 	Assign(typ, id, exp) -> write_assign id exp typ
 
-let write_assign i e t = 
+and write_assign i e t = 
 	match e with 
 	_ -> write_type t ^ " " ^ i ^ " = " ^ write_expr e
 
-let write_type ty =
+and write_type ty =
 	match ty with
 	Int -> "int"
 	| String -> "String"
@@ -46,23 +46,24 @@ let write_type ty =
 
 
 
-let write_expr e = 
+and write_expr e = 
 	match e with
 	IntLit(i, t) -> string_of_int i
 	| StringLit(str, t) -> "\"" ^ str ^ "\""
 	| Id(x, t) -> x
-	| Binop(e_1, op, e_2, t) -> write_bin_op e_1 op e_2 t
+(*	| Binop(e_1, op, e_2, t) -> write_bin_op e_1 op e_2 t *)
+	| Binop(e_1, op, e_2) -> write_bin_op e_1 op e_2
 	| BasicList(l) -> "[" ^ List.map (fun e -> write_expr e ^ "," ) l ^  "]"
 	| Note_S(nt, dr) -> "new Note(" ^ string_of_int nt ^ ", " ^ write_rhythm dr  ^ ")"
 	| FuncList(funk_args, l) -> (List.map2 map_calls funk_args l) ^ write_expr l
 
 
-let mapcall func param = 
+and mapcall func param = 
 	match func with
 		"print" -> "System.out.println(" ^ param ^ ");\n"
 		| "play" -> "Play.midi(" ^ write_expr param ^ ");\n"
 
-let write_rhythm dr =
+and write_rhythm dr =
 			match dr with 
 			S -> "0.25"
 			| E -> "0.5"
@@ -71,6 +72,20 @@ let write_rhythm dr =
 			| W -> "2.0"
 
 
+(* This is a replacement from Fry *)
+and writeBinOp e1 o e2 = 
+	match o with 
+	Equal -> write_expr e1 ^".equals("^ write_expr e2^") " 
+	| Neq -> "!" ^ write_expr e1 ^".equals("^ write_expr e2^") " 
+	| _ -> j_expr e1 ^ 
+		(match o with Plus -> "+" | Minus -> "-" | Times -> "*" |
+				  Divide -> "/" |  
+				  Less -> "<" | Leq -> "<=" | Greater -> ">" |
+				  Geq -> ">=" (* |  In -> "In" | Notin -> "Notin" | From -> "From" *)
+				 (* | And -> "&&" | Or -> "||" *)) ^ write_expr e2
+(*
+
+	I don't think this will work becasue we don't have types in writeBinOp in AST
 
 let write_bin_op ex1 op ex2 typ = 
 	let e1 = write_expr ex1 and e2 = write_expr ex2 in
@@ -80,11 +95,11 @@ let write_bin_op ex1 op ex2 typ =
             Less | Leq | Greater | Geq) -> e1 ^ write_op_primitive op ^ e2)
             | Note -> e1 ^ ".setPitch( " ^ e1 ^ ".getPitch() + " ^ e2 ^
             ".getPitch());\n"
-            Note n = new Note(C4+i, CROTCHET);
+            Note n = new Note(C4+i, CROTCHET); *)
 
 (* Functions below were copied from Corgi - Fall 2014 *)
 
-let write_op_primitive = function
+and write_op_primitive = function
 	Plus -> " + "
 	| Minus -> " - "
 	| Times -> " * "
@@ -107,3 +122,4 @@ let write_op_compares e1 op e2 =
 	| Neq -> "(" ^ e1 ^ ").compareTo(" ^ e2 ^ ")" ^ " != 0"
 	| _ -> raise (Failure "not a comparator operation")
 *)
+
