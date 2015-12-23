e6b0a257883080fbe9d841e963076f34a321b732
Savvas Petridis <sdp2137@columbia.edu>
2015-12-22 22:54:58 -0500

    cleaner table/compiler

--- marmalade.ml
+++ marmalade.ml
@@ -7,7 +7,7 @@ open Printf
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
-  let env = Table.build_table program in
+  let env = Table.create_table program in
   let sast_pgm = Sast.confirm_semantics program env in
   let compiled_program = (*Compile.to_java program Sys.argv.(1)*) Javagen.gen_pgm sast_pgm Sys.argv.(1) in 
      let file = open_out (Sys.argv.(1) ^ ".java") in
--- table.ml
+++ table.ml
@@ -1,85 +1,93 @@
 (* 
- *	Creates table for checking SAST
- *	much of this was adapted from Corgi
+ * table.ml of marmalade
+ * Creates table for checking SAST
  *)
 
 open Ast
 
 module StrMap = Map.Make(String)
 
-let env_table (table,_) = table
-let env_scope (_,scope) = scope
+let table_env (table,_) = table
+let scope_env (_,scope) = scope
 let type_of_funct_args (_,_,p_type) = p_type
 
-let parent_scope = Array.make 1000 0
+let over_scope = Array.make 1000 0
 
-let rec map_to_list_env func lst env =
+let rec map func lst env =
 	match lst with
 		  [] -> env
 		| head :: tail ->
 			let new_env = func head env in 
-				map_to_list_env func tail new_env
+				map func tail new_env
+
 
 let name_scope_str (name:string) env =
-	name ^ "_" ^ (string_of_int (env_scope env))
+	name ^ "_" ^ (string_of_int (scope_env env))
 
 let rec get_scope name env =
     if StrMap.mem (name_scope_str name env) (fst env) then (snd env)
     else if (snd env) = 0 then raise(Failure("Error: Symbol " ^ name ^ " not declared. " ^ string_of_int (snd env)))
-    else get_scope name (fst env, parent_scope.(snd env))
+    else get_scope name (fst env, over_scope.(snd env))
 
 let rec get_decl name env =
 	let key = name_scope_str name env in 
 	if StrMap.mem key (fst env) then StrMap.find key (fst env)
 	else
 		if (snd env) = 0 then raise (Failure("Error: Symbol " ^ name ^ " not declared in current scope" ^ string_of_int (snd env) ^ "."))
-    	else get_decl name ((fst env), parent_scope.(snd env))
+    	else get_decl name ((fst env), over_scope.(snd env))
 
-let add_symbol (name:string) (decl:decl) env =
+let insert_symb (name:string) (decl:decl) env =
 	let key = name_scope_str name env in
-    if StrMap.mem key (env_table env)
+    if StrMap.mem key (table_env env)
     then raise(Failure("Error: Symbol " ^ name ^ " declared twice in same scope."))
-    else ((StrMap.add key decl (env_table env)), (env_scope env))
+    else ((StrMap.add key decl (table_env env)), (scope_env env))
 
-let add_var var env =
+let insert_var var env =
 	let (name, p_type) = var in
 	let is_implicit_array = 
 		(match p_type with
 		  (Int | Note | String | TimeSig | Instr | Tempo) -> false
-		  | _ -> true) in add_symbol name (Var_Decl(name, is_implicit_array, p_type, (env_scope env))) env
+		  | _ -> true) in insert_symb name (Var_Decl(name, is_implicit_array, p_type, (scope_env env))) env
 
-let add_ast_var var env =
+let insert_astvar var env =
 	let (name, arr_b, typ) = var in
-	add_var (name, typ) env
+	insert_var (name, typ) env
 
-let rec add_stmt stmt env =  
+(* insert stmt - matches first, then inserts *)
+
+let rec insert_stmt stmt env =  
 	(match stmt with
 	Expr(exp) -> env
-	| If(e, bl_1, bl_2) -> let env_1 = add_block bl_1 Wild env in add_block bl_2 Wild env_1
-	| While(e, bl) -> add_block bl Wild env
-	| Fdecl(fdec) -> add_func fdec env
+	| If(e, bl_1, bl_2) -> let env_1 = insert_code_block bl_1 Wild env in insert_code_block bl_2 Wild env_1
+	| While(e, bl) -> insert_code_block bl Wild env
+	| Fdecl(fdec) -> insert_funk fdec env
 	| VarDecl(chan) -> (match chan with 
-		Assign(typ, id, blah) -> add_var (id, typ) env
-	 	| Update(str, exr) -> env 
+		Assign(typ, id, blah) -> insert_var (id, typ) env
+	 	| Update(str, exr) ->  env
 		| Index_Update(_, _) -> env)
 	| _ ->	env )
 
+(* insert contents of a block of code *)
 
-and add_block block return_tp env =  
+and insert_code_block block return_tp env =  
 	let (table, scope) = env in 
 	let id = block.block_id in
-	let env = map_to_list_env add_ast_var block.locals (table, id) in
-	let env = map_to_list_env add_stmt block.statements env in
-	parent_scope.(id) <- scope; 
-	((env_table env), scope)
+	let env = map insert_astvar block.locals (table, id) in
+	let env = map insert_stmt block.statements env in
+	over_scope.(id) <- scope; 
+	((table_env env), scope)
 
-and add_func func env =
+(* insert contents of a function into the table *)
+
+and insert_funk func env =
 	let (table, scope) = env in
 	let arg_names = List.map type_of_funct_args func.args in
-	let env = add_symbol func.fname (Func_Decl(func.fname, func.ret_type, func.f_type, arg_names, scope)) env in
-	add_block func.body (func.ret_type) ((env_table env), scope)
+	let env = insert_symb func.fname (Func_Decl(func.fname, func.ret_type, func.f_type, arg_names, scope)) env in
+	insert_code_block func.body (func.ret_type) ((table_env env), scope)
 
-let base_env = 
+(* initialize start_env *)
+
+let start_env = 
 	let table = StrMap.add "print_0" (Func_Decl("print", Null_Type, [Int; Note;
     String; Song; Phrase; Measurepoo; TimeSig; Instr; Tempo; List ; Intlist ; Stringlist; Wild], [], 0)) StrMap.empty in
     let table = StrMap.add "evaluate_note_0" (Func_Decl("evaluate_note", Note, [Int; Note;
@@ -90,6 +98,7 @@ let base_env =
     String; Song; Phrase; Measurepoo; TimeSig; Instr; Tempo; List ; Intlist ; Stringlist; Wild], [Phrase], 0)) table in
     let table = StrMap.add "evaluate_song_0" (Func_Decl("evaluate_song", Song, [Int; Note;
     String; Song; Phrase; Measurepoo; TimeSig; Instr; Tempo; List ; Intlist ; Stringlist; Wild], [Song], 0)) table in
+    let table = StrMap.add "length_note_0" (Func_Decl("length_mnote", Int, [Measurepoo; Note; Phrase; Song; Intlist; Stringlist], [Note], 0)) table in
     let table = StrMap.add "length_measure_0" (Func_Decl("length_measure", Int, [Measurepoo; Note; Phrase; Song; Intlist; Stringlist], [Measurepoo], 0)) table in
     let table = StrMap.add "length_phrase_0" (Func_Decl("length_phrase", Int, [Measurepoo; Note; Phrase; Song; Intlist; Stringlist], [Phrase], 0)) table in
     let table = StrMap.add "length_song_0" (Func_Decl("length_measure", Int, [Measurepoo; Note; Phrase; Song; Intlist; Stringlist], [Song], 0)) table in
@@ -100,10 +109,11 @@ let base_env =
 	let table = StrMap.add "main_0" (Func_Decl("main", Null_Type, [], [], 0)) table in
 	(table, 0)
 
-let build_table p = 
-	let env = base_env in
-	let env =  map_to_list_env add_stmt (List.rev p.stmts) env in
-	let env = map_to_list_env add_func (List.rev p.funcs) env in 
-	let () = Printf.printf "-----------through table------------ \n" in 
-	env
+(* main function in this file -- initiates table, inserts statements and funks *)
 
+let create_table p = 
+	let env = start_env in
+	let env =  map insert_stmt (List.rev p.stmts) env in
+	let env = map insert_funk (List.rev p.funcs) env in 
+	let () = Printf.printf "// Symbol Table Created" in 
+	env
